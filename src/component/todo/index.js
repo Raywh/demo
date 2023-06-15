b12fa40cc4ea5d4d39b94b80138ef260b40ca049
wanghu <wanghu@jumpy.ai>
2023-06-15 19:21:30 +0800

    使用eject 配置别名@

--- src/component/todo/index.js
+++ src/component/todo/index.js
@@ -6,22 +6,27 @@ import { generateRandomId } from '@/utils'
 export default function TodoList() {
   const [title, setTitle] = useState('')
   const [list, setList] = useState([])
-  const listItems = list.map(person => <li>{person}</li>);
+  const listItems = list.map(obj => <li key={obj.id}>{obj.text}</li>);
 
   const handleAdd = () => {
-    console.log(list)
+    if (title === '') return
     const id = generateRandomId(10)
     console.log(id)
-
-    list.push(title)
+    const obj = {
+      id: id,
+      text: title
+    }
+    list.push(obj)
     setList(list)
+    console.log(list)
+
   }
   return (
     <div className="container">
       <h3>TodoList</h3>
 
       <div className='create'>
-        <Input placeholder="Basic usage" value={title} />
+        <Input placeholder="Basic usage" value={title} onChange={(e) => {setTitle(e.target.value)}} />
         <Button ghost onClick={handleAdd}>Default</Button>
       </div>
       <div className='list'>
